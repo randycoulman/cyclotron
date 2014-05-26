1775a07d31d9d995572d2e6874e68ef2e33af311
Randy Coulman <rcoulman@gmail.com>
2014-05-26 16:50:21 -0700

    Add Route tests and validations

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,2 +1,3 @@
 class Route < ActiveRecord::Base
+  validates :name, presence: true, length: {maximum: 50}
 end
--- test/models/route_test.rb
+++ test/models/route_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class RouteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  attr_reader :route
+
+  def setup
+    @route = Route.new(name: "Favorite", description: "My favorite route")
+  end
+
+  def test_has_a_name
+    assert_equal "Favorite", route.name
+  end
+
+  def test_has_a_description
+    assert_equal "My favorite route", route.description
+  end
+
+  def test_is_valid
+    assert route.valid?
+  end
+
+  def test_must_have_a_name
+    route.name = "  "
+    refute route.valid?
+  end
+
+  def test_name_cant_be_too_long
+    route.name = "a" * 51
+    refute route.valid?
+  end
+
+  def test_allows_blank_description
+    route.description = ""
+    assert route.valid?
+  end
 end
