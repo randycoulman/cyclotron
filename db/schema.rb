3d4c40c37fa6332862c51407f2994eaa4e03987d
Randy Coulman <rcoulman@gmail.com>
2014-05-26 14:56:03 -0700

    Generate scaffold for routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526215129) do
+ActiveRecord::Schema.define(version: 20140526215324) do
 
   create_table "bikes", force: true do |t|
     t.string "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "routes", force: true do |t|
+    t.string "name"
+    t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
