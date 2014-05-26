f75575752bedde6f17b8f1f689ae25ff4902a87a
Randy Coulman <rcoulman@gmail.com>
2014-05-26 16:50:06 -0700

    Add Bike tests and validations

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,2 +1,3 @@
 class Bike < ActiveRecord::Base
+  validates :name, presence: true, length: {maximum: 30}
 end
--- test/models/bike_test.rb
+++ test/models/bike_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class BikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  attr_reader :bike
+
+  def setup
+    @bike = Bike.new(name: 'Speed Racer')
+  end
+
+  def test_has_a_name
+    assert_equal "Speed Racer", bike.name
+  end
+
+  def test_is_valid
+    assert bike.valid?
+  end
+
+  def test_must_have_a_name
+    bike.name = " "
+    refute bike.valid?
+  end
+
+  def test_name_cant_be_too_long
+    bike.name = "a" * 31
+    refute bike.valid?
+  end
 end
