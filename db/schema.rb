1228823a2fddf6b9368b366b4b5569756d717a1c
Randy Coulman <rcoulman@gmail.com>
2014-05-26 20:30:39 -0700

    Format schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -14,28 +14,28 @@
 ActiveRecord::Schema.define(version: 20140526220248) do
 
   create_table "bikes", force: true do |t|
-    t.string "name"
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "rides", force: true do |t|
-    t.integer "bike_id"
-    t.integer "route_id"
-    t.date "ride_date"
-    t.decimal "distance", precision: 6, scale: 3
-    t.decimal "speed", precision: 3, scale: 1
+    t.integer  "bike_id"
+    t.integer  "route_id"
+    t.date     "ride_date"
+    t.decimal  "distance",   precision: 6, scale: 3
+    t.decimal  "speed",      precision: 3, scale: 1
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.text "notes"
+    t.text     "notes"
   end
 
   add_index "rides", ["bike_id"], name: "index_rides_on_bike_id"
   add_index "rides", ["route_id"], name: "index_rides_on_route_id"
 
   create_table "routes", force: true do |t|
-    t.string "name"
-    t.text "description"
+    t.string   "name"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
