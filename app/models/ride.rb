c8611dbf1e29119e8047778a9eb29c314416b849
Randy Coulman <rcoulman@gmail.com>
2014-05-26 17:21:33 -0700

    Add Ride tests and validations

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,10 @@
 class Ride < ActiveRecord::Base
   belongs_to :bike
   belongs_to :route
+
+  validates :bike_id, presence: true
+  validates :route_id, presence: true
+  validates :ride_date, presence: true
+  validates :distance, numericality: {greater_than: 0}
+  validates :speed, numericality: {greater_than: 0}
 end
--- test/models/ride_test.rb
+++ test/models/ride_test.rb
@@ -1,7 +1,70 @@
 require 'test_helper'
 
 class RideTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  attr_reader :ride, :bike, :route
+
+  def setup
+    @bike = Bike.create!(name: "A bike")
+    @route = Route.create!(name: "A route")
+    @ride = Ride.new(bike: @bike, route: @route, ride_date: Date.today, distance: 12.345, speed: 16.7)
+  end
+
+  def test_knows_its_bike
+    assert_equal bike, ride.bike
+  end
+
+  def test_knows_its_route
+    assert_equal route, ride.route
+  end
+
+  def test_has_a_ride_date
+    assert_equal Date.today, ride.ride_date
+  end
+
+  def test_has_a_distance
+    assert_equal(12.345, ride.distance)
+  end
+
+  def test_has_a_speed
+    assert_equal(16.7, ride.speed)
+  end
+
+  def test_is_valid
+    assert ride.valid?
+  end
+
+  def test_must_have_a_bike
+    ride.bike_id = nil
+    refute ride.valid?
+  end
+
+  def test_must_have_a_route
+    ride.route_id = nil
+    refute ride.valid?
+  end
+
+  def test_must_have_a_ride_date
+    ride.ride_date = nil
+    refute ride.valid?
+  end
+
+  def test_must_have_a_numerical_distance
+    ride.distance = "not a number"
+    refute ride.valid?
+  end
+
+  def test_must_have_a_positive_distance
+    ride.distance = 0
+    refute ride.valid?
+  end
+
+  def test_must_have_a_numerical_speed
+    ride.speed = "not a number"
+    refute ride.valid?
+  end
+
+  def test_must_have_a_positive_speed
+    ride.speed = 0
+    refute ride.valid?
+  end
 end
